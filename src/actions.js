1cd61e5e1c2e2750fd6ad0504a5b06c6775d2083
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-04-18 16:56:54 -0700

    Continue setting up login action and use axios to send login or register request to to-do API

--- src/actions.js
+++ src/actions.js
@@ -1,24 +1,51 @@
 import { redirect } from "react-router-dom";
-
+import axios from "axios";
 import {listsList} from './FAKEBACKEND'
+
+const toDoAPIBaseURL = "http://localhost:3000";
 
 export async function loginAction({ request, /*params*/ }) {
   const formData = await request.formData();
-  //the body of the form is available under formData.get(nameOfInputField)
-  //http method available under request.method
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const user = JSON.stringify({email: email,password: password});
+  const headers = { "Content-Type": "application/json" };
 
   // if there is password validating then they are creating an account, otherwise login
   if(formData.get('passwordValidation')){
-    console.log(formData.get("passwordValidation"));
-  }
-  //here is where we would make the post request to our backend
-  if (formData.get("password")==1) {
-    return redirect('/home/lists');
-  } else {
-    return true;
-  }
-    // once we get a response we can tell our react app to redirect to another page
+
+    // check that what user typed in for password equals what they typed in for password validation
+    const passwordVal = formData.get('passwordValidation');
+    if(password === passwordVal) {
+      try {
+        const response = await axios.post(`${toDoAPIBaseURL}/register`, user, {headers:headers});
+        const res = response.data;
+        console.log(res);
+      } catch (error) {
+        console.log(`error:` + error);
+      }
+    } else {
+      return true;
+    }
     
+    
+  } else {
+
+    // they are making a login request
+    
+    try {
+      const response = await axios.post(`${toDoAPIBaseURL}/login`, user, {headers: headers,});
+      const res = response.data;
+
+      console.log(res);
+      // return redirect("/home/lists");
+      return true;
+    } catch (error) {
+      console.log(`error:` + error);
+    }
+  }
+  
+  return true;  
 }
 
 export async function newListAction({ request /*params*/ }) {

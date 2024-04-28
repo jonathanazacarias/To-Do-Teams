89476f1e197ccbbbd23abdfba847661c1bcee97e
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-04-28 09:47:16 -0700

    Fix ability to add a new list and edit lists

--- src/utils/AuthProvider.jsx
+++ src/utils/AuthProvider.jsx
@@ -1,18 +1,26 @@
 import { useState, } from "react";
-import {PropTypes} from "prop-types";
+import { PropTypes } from "prop-types";
 import { AuthContext } from "./contexts";
 import { Outlet } from "react-router-dom";
-
+import Cookie from 'js-cookie';
+import Cookies from "js-cookie";
 
 
 export function AuthProvider() {
     const [user, setUser] = useState(null);
+    
+    if(Cookies.get("user")) {
+        // setUser(Cookies.get("user"));
+        console.log(Cookies.get("user"));
+    }
 
     function login(user) {
+        Cookie.set("user", user, {expires: 1});
         setUser(user);
     }
 
     function logout() {
+        Cookies.remove("user");
         setUser(null);
     }
 

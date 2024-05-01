f4033709a3f41152a62a7ee8f5c87f708f8ebe5f
Jonathan Zacarias <jonathanazacarias@gmail.com>
2024-05-01 14:45:55 -0700

    Finish the registration api call and load lists specific to the user.

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import { Form, Link, useActionData} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Form, Link, useActionData, useNavigate} from 'react-router-dom'
 import NavBar from '../components/nav/NavBar';
+import { useAuth } from '../utils/contexts';
 
 
 export default function Register() {
+    const navigate = useNavigate();
+    const auth = useAuth();
+
     const [showPassword, toggleShowPassword] = useState(false);
     const [showValPassword, toggleShowValPassword] = useState(false);
 
-    const invalidRegistration = useActionData();
-    if (invalidRegistration) {
-        console.log(`Do something to indicate incorrect registration`);
-    }
+    const registrationResult = useActionData();
+    
+
+    useEffect(() => {
+        if (registrationResult) {
+            if (registrationResult.error) {
+                console.log(registrationResult.error.response.data);
+            } else {
+                console.log(registrationResult);
+                auth.login(registrationResult);
+                navigate("../lists");
+            }
+        }
+    }, [registrationResult, auth, navigate])
 
     let createWords = {
         header: 'Create Account',
@@ -37,14 +51,14 @@ export default function Register() {
             <h1>{ createWords.header}</h1>
             <Form method='post' >
                 <label htmlFor="email">Email</label>
-                <input type='text' name='email' placeholder='email' autoComplete='email' />
+                <input type='text' name='email' placeholder='email' autoComplete='email' required/>
                 <label htmlFor='username'>Display Name</label>
-                <input type='text' name='username' placeholder='username' autoComplete='new-username' />
+                <input type='text' name='username' placeholder='username' autoComplete='new-username' required/>
                 <label htmlFor='password'>Password</label>
-                <input type={showPassword ? 'text':'password'} name='password' placeholder='password' autoComplete='new-password' />
+                <input type={showPassword ? 'text':'password'} name='password' placeholder='password' autoComplete='new-password' required/>
                 <button onClick={handleShowPassword} >{showPassword ? 'Hide':'Show'}</button>
                 <label htmlFor='passwordValidation'>Retype Password</label>
-                <input type={showValPassword ? 'text' : 'password'} name='passwordValidation' placeholder='password' autoComplete='off' />
+                <input type={showValPassword ? 'text' : 'password'} name='passwordValidation' placeholder='password' autoComplete='off' required/>
                 <button onClick={handleShowValPassword} >{showValPassword ? 'Hide' : 'Show'}</button>
                 <button type='submit'>{createWords.formButton}</button>
             </Form >
